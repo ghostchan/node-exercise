f865690f87aaf6d8c614070fd71990bc8215e5cd
ghostchan <jinnchan@qq.com>
2017-07-27 00:11:04 +0800

    request response

--- 3.http/1.http.js
+++ 3.http/1.http.js
@@ -1,3 +1,15 @@
 var http = require('http')
-var server = http.createServer();
+function serve(request,response) {
+    console.log(request.method);
+    console.log(request.url);//请求里的URL地址
+    console.log(request.headers);//获取请求头
+
+    response.statusCode = 404;//设置状态码
+    //设置响应的类型，编码为utf-8
+    response.setHeader('Content-Type','text/html;charset=utf-8')
+    response.setHeader('name','test');//设置响应头
+    response.write(new Date().toString());
+    response.end();
+}
+var server = http.createServer(serve);
 server.listen(8080,'localhost');
