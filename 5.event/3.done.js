942b67b9ccd583ca77adfac206aabfd94459e4c0
ghostchan <jinnchan@qq.com>
2017-08-16 00:00:53 +0800

    events

--- 5.event/3.done.js
+++ 5.event/3.done.js
@@ -1,18 +1,18 @@
 var fs = require('fs');
-var person ={};
-var count =0;
-fs.readFile('name.txt','utf8',function (err,data) {
+var person = {};
+var count = 0;
+fs.readFile('name.txt', 'utf8', function (err, data) {
     person.name = data;
-    if(++count==2){
+    if (++count == 2) {
         out();
     }
 });
-fs.readFile('age.txt','utf8',function (err,data) {
+fs.readFile('age.txt', 'utf8', function (err, data) {
     person.age = data;
-    if(++count==2){
+    if (++count == 2) {
         out();
     }
 });
 function out() {
-    console.log(person.name,person.age);
+    console.log(person.name, person.age);
 }
--- 5.event/3.eventdone.js
+++ 5.event/3.eventdone.js
@@ -2,17 +2,17 @@ var EventEmitter = require('events');
 
 var eve = new EventEmitter();
 var fs = require('fs');
-var person ={};
-eve.on('data',out);
-fs.readFile('name.txt','utf8',function (err,data) {
+var person = {};
+eve.on('data', out);
+fs.readFile('name.txt', 'utf8', function (err, data) {
     person.name = data;
     eve.emit('data');
 });
-fs.readFile('age.txt','utf8',function (err,data) {
+fs.readFile('age.txt', 'utf8', function (err, data) {
     person.age = data;
     eve.emit('data');
 });
 function out() {
-    if(person.name && person.age)
-    console.log(person.name,person.age);
+    if (person.name && person.age)
+        console.log(person.name, person.age);
 }
